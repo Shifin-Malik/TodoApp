5c76538b4e920d0682fdd195ab1740815f32dd12
Shifin-Malik <shifinmalik46@gmail.com>
2024-12-09 10:33:05 -0800

    first-commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "./app.css";
+  import TodoApp from "$lib/components/TodoApp.svelte";
+</script>
+
+<main>
+ 
+    <div class="flex flex-col items-center pt-20 h-screen bg-gray-900 ">
+    
+      <TodoApp />   
+   
+  </div>
+</main>
